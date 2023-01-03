1ca492e7d3885a786dea6d6b0926cf832a9eeda3
Kamalakannan Anandan <kamalakannan.anandan@gmail.com>
2023-01-03 11:55:25 -0600

    update

--- RestAssuredAPITesting/src/test/java/TC001_GET_Request.java
+++ RestAssuredAPITesting/src/test/java/TC001_GET_Request.java
@@ -2,8 +2,10 @@ import java.lang.reflect.Method;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 
 public class TC001_GET_Request {
 	
@@ -12,30 +14,35 @@ public class TC001_GET_Request {
 		
 		//Specify Base URI
 		RestAssured.baseURI = "https://rahulshettyacademy.com/maps/api/place/get/json";
-				
-		//Request object
-		RequestSpecification httpRequest = RestAssured.given();
 		
-		//Response Object
-		Response response = httpRequest.request(Method.GET,"qaclick123");
-				
-		/*Response response = given().queryParam("key","qaclick123")
-        .queryParam("place_id","d3689a4d7a7ed0d8ab97535cb8036e2e")
-        .when().get("https://rahulshettyacademy.com/maps/api/place/get/json").then().log().body();
-        */
+		ValidatableResponse response = RestAssured
+				.given()
+					.param("", "")
+					.header("","")
+				.when()
+					.get("qaclick123")
+				.then()
+					.statusCode(200);
 		
-		String responseBody=response.getBody().asString();
-		System.out.println("Response Body: " +responseBody);
-		
-		//status code verification
-		int statusCode=response.getStatusCode();
-		System.out.println("Status Code: " +statusCode);
-		Assert.assertEquals(statusCode, 200);
-		
-		//status line verification
-		String statusLine=response.statusLine();
-		System.out.println("Status Line: " +statusLine);
-		Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+		/*
+		 * //Request object RequestSpecification httpRequest = RestAssured.given();
+		 * 
+		 * //Response Object Response response =
+		 * httpRequest.request(Method.GET,"qaclick123");
+		 * 
+		 * 
+		 * 
+		 * String responseBody=response.getBody().asString();
+		 * System.out.println("Response Body: " +responseBody);
+		 * 
+		 * //status code verification int statusCode=response.getStatusCode();
+		 * System.out.println("Status Code: " +statusCode);
+		 * Assert.assertEquals(statusCode, 200);
+		 * 
+		 * //status line verification String statusLine=response.statusLine();
+		 * System.out.println("Status Line: " +statusLine);
+		 * Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+		 */
 		
 	}
 
